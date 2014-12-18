dc20510b10606a9021c34653ab5f73ddcaadffda
Piotr Szulc <szulo123@gmail.com>
2014-12-18 22:05:45 +0100

    Drawing array of tiles

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -12,35 +12,73 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        public TileMap Map { get; private set; }
+        private Map GameMap { get; set; }
 
         public Form1()
         {
             InitializeComponent();
-            this.Map = new TileMap(Properties.Resources.grass_dirt, 32, 32);
+
+            this.GameMap = new Map(new TiledBitmap(Properties.Resources.grass_dirt, 32, 32));
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             var g = e.Graphics;
-            g.DrawImage(Map.Tiles[4], 0, 0);
-            g.DrawImage(Map.Tiles[4], 32, 0);
+            GameMap.Draw(g);
+        }
+
+    }
+
+    public class Map
+    {
+        public TiledBitmap tileMap { get; set; }
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        private int[,] tiles;
+
+        public Map(TiledBitmap tileMap)
+        {
+            this.tileMap = tileMap;
+
+            this.Width = 5;
+            this.Height = 5;
+            this.tiles = new int[,] {
+                { 0,  0,  0,  0,  0 },
+                { 3,  4,  4,  4,  5 },
+                { 6,  7,  7,  7,  8 },
+                { 9, 10, 10, 10, 11 },
+                { 0,  0,  0,  0,  0 },
+            };
+        }
+
+        public void Draw(Graphics g)
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    var t = tiles[y,x];
+                    g.DrawImage(tileMap.Tiles[t], x * tileMap.TileWidth, y * tileMap.TileHeight);
+                }
+            }
         }
     }
 
-    public class TileMap
+    public class TiledBitmap
     {
         public Bitmap SourceBitmap { get; private set; }
         public List<Bitmap> Tiles { get; private set; }
 
-        private int tileWidth, tileHeight;
+        public int TileWidth { get; private set; }
+        public int TileHeight { get; private set; }
 
-        public TileMap(Bitmap bitmap, int tileWidth, int tileHeight)
+        public TiledBitmap(Bitmap bitmap, int tileWidth, int tileHeight)
         {
             this.SourceBitmap = bitmap;
-            this.tileWidth = tileWidth;
-            this.tileHeight = tileHeight;
-
+            this.TileWidth = tileWidth;
+            this.TileHeight = tileHeight;
             this.Tiles = SplitIntoTiles(bitmap, tileWidth, tileHeight);
         }
 
